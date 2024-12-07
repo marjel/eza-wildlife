ca7ab50cb9cf1f0d7b655c33f846e8a354881f87
marjel <marjel73@gmail.com>
2024-12-07 18:51:12 +0100

    menu animation

--- src/app/components/menu-link/menu-link.component.ts
+++ src/app/components/menu-link/menu-link.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
 import { MenuLink } from '../../model/menu-link.model';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu-link',
@@ -9,24 +10,46 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [CommonModule, MatButtonModule],
   templateUrl: './menu-link.component.html',
   styleUrls: ['./menu-link.component.scss'],
+  animations: [
+    trigger('submenu', [
+      state('closed', style({ height: '0', overflow: 'hidden', opacity: 0 })),
+      state('open', style({ height: '*', opacity: 1 })),
+      transition('closed <=> open', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class MenuLinkComponent {
   @Input() link!: MenuLink;
   @Output() linkClicked = new EventEmitter<string>();
-  isOpen = false; // Kontroluje widoczność submenu
+  isOpen = false;
+
+  constructor(private elementRef: ElementRef) {}
 
   toggleSubmenu(): void {
     this.isOpen = !this.isOpen;
-    console.log('isOpen:', this.isOpen); // Debugging
+  }
+
+  closeSubmenu(): void {
+    this.isOpen = false;
   }
 
   onLinkClick(): void {
     if (!this.link.submenu) {
       this.linkClicked.emit(this.link.value);
+      this.closeSubmenu();
     }
   }
 
   onSubLinkClicked(value: string): void {
     this.linkClicked.emit(value);
+    this.closeSubmenu();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (!this.elementRef.nativeElement.contains(target)) {
+      this.closeSubmenu();
+    }
   }
 }
