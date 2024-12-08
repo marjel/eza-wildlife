d57d1e7c0f46309158be5e889f1f960e49807e89
marjel <marjel73@gmail.com>
2024-12-08 17:39:17 +0100

    menu fixed

--- src/app/components/main-tool-bar/main-tool-bar.component.ts
+++ src/app/components/main-tool-bar/main-tool-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, inject, OnInit } from '@angular/core';
+import { Component, computed, effect, HostBinding, inject, OnInit } from '@angular/core';
 import { AppFacade } from '../../service/app.facade';
 import { MenuComponent } from '../menu/menu.component';
 import { CommonModule } from '@angular/common'; // Dodaj CommonModule dla dyrektyw, takich jak *ngIf
@@ -16,6 +16,12 @@ export class MainToolBarComponent implements OnInit {
 
   protected appFacade = inject(AppFacade);
   protected structure = computed(() => this.appFacade.structure());
+
+  @HostBinding('style.height') height = 'auto';
+
+  adjustHeight(hasOpenSubmenu: boolean): void {
+    this.height = hasOpenSubmenu ? '300px' : 'auto'; // Dostosuj wysokość
+  }
 
   ngOnInit(): void {
     this.appFacade.loadStructure();
