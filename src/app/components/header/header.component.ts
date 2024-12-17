fbed0c6c9503576cfb79e9c06024736cb1df4428
marjel <marjel73@gmail.com>
2024-12-17 22:11:31 +0100

    slideshow styles install gasp

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -11,12 +11,46 @@ import { register } from 'swiper/element/bundle';
   imports: [MatTooltipModule, TranslateModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent  implements AfterViewInit {
 
   ngAfterViewInit() {
-    // Rejestracja elementów Swiper
+     const swiperEl = document.querySelector('swiper-container');
+
+     const params = {
+      injectStyles: [`
+      .swiper-pagination-bullet {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 12px;
+        color: #000;
+        opacity: 1;
+        background: rgba(0, 0, 0, 0.2);
+      }
+      .swiper-pagination-bullet-active {
+        color: #fff;
+        background: #ff7a00;
+      }
+      `],
+      pagination: {
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+    }
+
+    
+
+    if(swiperEl) {
+      Object.assign(swiperEl, params)
+    }
+  
+
     register();
   }
 
