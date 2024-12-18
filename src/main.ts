cd460e3bcc09fae1e6482c63b119d1f90719b0e0
marjel <marjel73@gmail.com>
2024-12-18 10:04:58 +0100

    add preloader

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient, HttpClient  } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
@@ -12,16 +12,32 @@ export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
+function hidePreloader() {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.display = 'none'; // Ukrycie preloadera
+  }
+  document.body.classList.add('loaded'); // Pokazanie app-root
+}
+
+// Bootstrap aplikacji
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(BrowserAnimationsModule, TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      }
-    })),
-  ]
-}).catch((err) => console.error(err));
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
+})
+  .then(() => {
+    setTimeout(hidePreloader, 7000)
+  })
+  .catch((err) => console.error(err));
